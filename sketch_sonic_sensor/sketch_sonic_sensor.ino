779e258cd113a4362862adb2c40085762d5e3df5
gnuradiodev <ed@CasitaPC.home>
2022-08-25 21:01:21 -0400

    freq min max

--- sketch_sonic_sensor/sketch_sonic_sensor.ino
+++ sketch_sonic_sensor/sketch_sonic_sensor.ino
@@ -6,8 +6,8 @@
 // Tested on 17 September 2019
 // ---------------------------------------------------------------- //
 
-#define echoPin 2 // attach pin D2 Arduino to pin Echo of HC-SR04
-#define trigPin 3 //attach pin D3 Arduino to pin Trig of HC-SR04
+#define echoPin 3 // attach pin D2 Arduino to pin Echo of HC-SR04
+#define trigPin 4 //attach pin D3 Arduino to pin Trig of HC-SR04
 
 #define lalarmPin 7 // attach pin D8 Arduino to pin Echo of HC-SR04
 #define lresetPin 8 //attach pin D9 Arduino to pin Trig of HC-SR04
@@ -16,7 +16,9 @@
 // defines variables
 long duration; // variable for the duration of sound wave travel
 int distance; // variable for the distance measurement
-int frequency;
+float frequency;
+int ldist;
+int hdist;
 float lfreq;
 float hfreq;
 
@@ -26,6 +28,7 @@ void setup() {
   pinMode(lalarmPin, OUTPUT); // Sets the alarmPin as an OUTPUT
   pinMode(lresetPin, OUTPUT); // Sets the resetPin as an INPUT
   pinMode(resetPin, INPUT_PULLUP); // Sets the resetPin as an INPUT
+  
   Serial.begin(9600); // // Serial Communication is starting with 9600 of baudrate speed
   Serial.println("Ultrasonic Sensor HC-SR04 Test"); // print some text in Serial Monitor
   Serial.println("with Arduino UNO R3");
@@ -33,7 +36,7 @@ void setup() {
 
 void reset()
 {
-  digitalWrite(alarmPin, LOW);
+  digitalWrite(lalarmPin, LOW);
   Serial.print("High Frequency: ");
   Serial.print(hfreq);
   Serial.print(" hz.....");
@@ -49,8 +52,11 @@ void reset()
   
   lfreq=0;
   hfreq=0;
-  ldur=0;
-  hdur=0;
+  ldist=0;
+  hdist=0;
+
+  //done with reset
+  digitalWrite(lresetPin, LOW);
 }
 
 void loop() {
@@ -59,8 +65,10 @@ void loop() {
   delayMicroseconds(2);
 
   
-  if (digitalRead(resetPin))
+  if (!digitalRead(resetPin))
   {
+    digitalWrite(lresetPin, HIGH);
+    delayMicroseconds(10);
     reset();
     while (digitalRead(resetPin));
   }
@@ -84,21 +92,65 @@ void loop() {
     ldist=distance;
   }
 
-  frequency = 1/duration;
   if (frequency > hfreq)
   {
     hfreq=frequency;
+    Serial.print("Max Freq: ");
+    Serial.print(hfreq);
+    Serial.println(" hz.   ");
   }
-  else if (frequency < lfreq)
+  else if (frequency < lfreq || lfreq ==0)
   {
-    lfreq = frequency;
+    lfreq = frequency;   
+    Serial.print("Min Freq: ");
+    Serial.print(lfreq);
+    Serial.println(" hz.   ");
   }
+
+  if (distance> hdist)
+  {
+    hdist = distance;
+    Serial.print("Max Dist: ");
+    Serial.print(hdist);
+    Serial.println(" cm.  ");
+  }
+  else if (distance < ldist || ldist==0)
+  {
+    ldist = distance; 
+    Serial.print("Min Dist: ");
+    Serial.print(ldist);
+    Serial.println(" cm.  ");
+  }
+
+
+  Serial.print("Max Freq: ");
+  Serial.print(hfreq);
+  Serial.print(" hz.   ");
+  Serial.print("Min Freq: ");
+  Serial.print(lfreq);
+  Serial.print(" hz.   ");
+  Serial.print("Max Dist: ");
+  Serial.print(hdist);
+  Serial.print(" cm.  ");
+  Serial.print("Min Dist: ");
+  Serial.print(ldist);
+  Serial.print(" cm.  ");
+
   
-    
+  Serial.print("Duration: ");
+  Serial.print(duration);
+  Serial.print(" s.        ");
+  
+  frequency = (1.0/(float)duration)*10000000.0;
+  
   Serial.print("Frequency: ");
   Serial.print(frequency);
-  Serial.println(" hz");
+  Serial.print(" hz.        ");
+  
   Serial.print("Distance: ");
   Serial.print(distance);
-  Serial.println(" cm");
+  Serial.println(" cm.");
+
+
+  
 }
