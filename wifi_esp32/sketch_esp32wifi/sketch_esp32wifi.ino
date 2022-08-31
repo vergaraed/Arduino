c1c5be98f21c5e93ae78f02845e07dc9562f57a2
Eddie V <blockchainbpi@gmail.com>
2022-08-31 18:02:33 -0400

    Esp32 Wifi query available networks, select/connect/prompt for password.  Needs a client Rest API

--- wifi_esp32/sketch_esp32wifi/sketch_esp32wifi.ino
+++ wifi_esp32/sketch_esp32wifi/sketch_esp32wifi.ino
@@ -1,7 +1,7 @@
 #include <WiFi.h>
 
 #define MAX_WIFI_NETWORKS 5
-#define WIFI_TIMEOUT_MS   20000
+#define WIFI_TIMEOUT_MS   2000
 void setup() {
   Serial.begin(115200);
 
@@ -152,15 +152,15 @@ void PrintMac()
 {
   WiFi.macAddress(mac);
   Serial.print("MAC: ");
-  Serial.print(mac[5],HEX);
-  Serial.print(":");
-  Serial.print(mac[4],HEX);
-  Serial.print(":");
-  Serial.print(mac[3],HEX);
-  Serial.print(":");
-  Serial.print(mac[2],HEX);
+  Serial.print(mac[0],HEX);
   Serial.print(":");
   Serial.print(mac[1],HEX);
   Serial.print(":");
-  Serial.println(mac[0],HEX);
+  Serial.print(mac[2],HEX);
+  Serial.print(":");
+  Serial.print(mac[3],HEX);
+  Serial.print(":");
+  Serial.print(mac[4],HEX);
+  Serial.print(":");
+  Serial.println(mac[5],HEX);
 }
